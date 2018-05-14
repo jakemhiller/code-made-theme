f7267827fb40946010e9322966d94022554ad586
Jake Hiller <jakemhiller@gmail.com>
2018-05-13 20:49:46 -0400

    update with more complex & organized syntax selectors

---
+++ examples/ex.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const React = require('react');
+
+class Test extends Component {
+  static defaultProps = {
+
+  }
+  render() {
+    return (
+      <div className={test}>
+
+      </div>
+    )
+  }
+}
+
+
+
+export default Test;
